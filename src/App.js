534ba49a4005273b951c1698d3869b67139a110d
Nasser-ALRuwais <nasserbnabdullah@hotmail.com>
2020-01-29 08:52:25 +0300

    fs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, im a react app</h1>
+        <Person name='Nasser' age='38'/>
+        <Person name="naif" age = '1'>Hobbies are gaming</Person>
+        <Person/>
       </div>
     );
+    // return React.createElement('div',{className: 'App'}, React.createElement('h1',null,'Header'));
   }
 }
 
